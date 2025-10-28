13049537b2a2e1e92ccd5d38476b1a58c2f01b33
Belal <belal@bm164.com>
2025-10-28 06:50:06 +0300

    routes: add app urlconf for inference endpoints (gallery, run_eval, upload)

--- inference_app/urls.py
+++ inference_app/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('upload/', views.upload_view, name='upload'),
     path('gallery/', views.gallery, name='gallery'),
-    path('run_eval/', views.run_eval, name='run_eval'),
+    path('upload_gallery/', views.upload_gallery, name='upload_gallery'),
+    path("run_eval_stream/", views.run_eval_stream, name="run_eval_stream"),
+    path("status_eval/", views.status_eval, name="status_eval"),
+    path("view_log/", views.view_log, name="view_log"),
+    path("cancel_eval/", views.cancel_eval, name="cancel_eval"),
 ]

94f0fd610bbcbc8dba20076f8aefab1deba0c94b
Belal <belal@bm164.com>
2025-10-28 06:50:00 +0300

    routes: add top-level urlconf pointing to inference_app

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('inference_app.urls')),
 ]
+
+# Serve /media/ -> MEDIA_ROOT
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve /results/ -> RESULTS_ROOT
+urlpatterns += static(settings.RESULTS_URL, document_root=settings.RESULTS_ROOT)
